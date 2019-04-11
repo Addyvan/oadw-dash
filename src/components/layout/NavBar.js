4ebb831e81ede0090e24aa47bb27068b775aa469
Beatrice084 <bjohn084@uottawa.ca>
2019-04-11 17:27:47 -0400

    Added first version of header

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Row, Col } from "reactstrap";
 
 import { I18n } from "react-i18next";
+import "./sidebar.css"; 
+import FlagEN from "./sig-en-w.png"
+import FlagFR from "./sig-en-w.png"
 
 import LanguageToggle from "../design-system/LanguageToggle";
 import '@gctools-components/aurora-css/css/aurora.min.css';
@@ -13,18 +16,9 @@ class NavBar extends React.Component {
       <I18n ns={["translation"]}>
         {
           (t, { i18n }) => (
-            <Row style={{paddingBottom: "5px", margin: "0px"}} className="oadw-nav">
-              <Col lg="2" md="3" sm="12" className="logo" style={{padding: "0px"}}>
-                <h1 style={{margin: "0px"}}>{t("title")} <div style={{fontSize: "9px"}}>(technical demonstration)</div></h1>
-              </Col>
-              <Col lg="10" md="9" sm="12" style={{padding: "5px"}} >
-                <Row>
-                  <Col lg="11" md="11" sm="10"></Col>
-                  <Col lg="1" md="1" sm="2" >
-                    <LanguageToggle i18n={ i18n }/>
-                  </Col>
-                </Row>
-              </Col>
+            <Row style = {{height: "60px", marginBottom: "0px", overflow: "hidden"}}>
+              <Col id = "slantedCol"><img id = "flag" src={FlagEN} alt="Government of Canada" /></Col>
+              <Col id = "gradient" style = {{width: "85%", backgroundColor: "blue", zIndex: "-1"}}></Col>
             </Row>
           )
         }
