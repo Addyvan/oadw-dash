ae942b40a765ec8d391431fa85c47be968287246
Beatrice084 <bjohn084@uottawa.ca>
2019-04-24 09:51:57 -0400

    Fixed mobile responsiveness of sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -50,73 +50,85 @@ class SideBar extends React.Component {
       <Container>
         <Row>
           <Col md="12" sm="12" lg="12" style = {{marginTop: "30px"}}>
+
             {/* GCanalytics header and divider */}
             <i style = {{color: "#137991", display: "inline", marginRight: "10px", marginLeft: "25px"}}class="fas fa-chart-bar fa-2x"></i>
             <h5 style={{display: "inline", fontFamily: 'Nunito Sans', fontWeight: "bold", color: "#4D5D6C"}}>GCanalytics</h5>
             <Nav style={{marginTop: "15px", display: "block"}} vertical>
               <DropdownItem divider></DropdownItem>
             </Nav>
+
             {/* Filter by drop down  */}
-            <div style = {{textAlign: "center", marginTop: "28px"}}>
-              <h6 style = {{display: "inline-block", color: "#707070"}}>Filter by</h6>
-              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} style = {{display: "inline-block", marginLeft: "5px"}}>
-                <DropdownToggle caret style = {{color: "#707070", backgroundColor: "#FFFFFF", borderColor: "#CECECE"}}>
-                  Filter by
-                </DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem>Foo Action</DropdownItem>
-                  <DropdownItem>Bar Action</DropdownItem>
-                  <DropdownItem>Quo Action</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-            </div>
-            {/* Date pickers */}
-            <Row style = {{marginTop: "27px"}}>
-              <Col md="12" sm="12" lg="6" style = {{display: "inline-block"}}>
-              <h6 style = {{color: "#707070", display: "inline-block"}}>From</h6>
-              <DatePickerInput
-                  displayFormat='DD/MM/YYYY'
-                  returnFormat='YYYY-MM-DD'
-                  className='my-react-component'
-                  placeholder='Start Date'
-                  defaultValue={this.state.startDate}
-                  iconClassName='calendar icon'
-                  validationFormat="DD/MM/YYYY"
-                  showOnInputClick={true}
-                  style = {{width: "30px", display: "inline-block", marginLeft: "5px"}}
-              />
-              </Col>
-              <Col md="12" sm="12" lg="6" style = {{display: "inline-block"}}>
-              <h6 style = {{color: "#707070", display: "inline-block"}}>To</h6>
-              <DatePickerInput
-                  displayFormat='DD/MM/YYYY'
-                  returnFormat='YYYY-MM-DD'
-                  className='my-react-component'
-                  placeholder='End Date'
-                  defaultValue={this.state.endDate}
-                  iconClassName='calendar icon'
-                  style={{ width: '200px', float: 'right' }}
-                  validationFormat="DD/MM/YYYY"
-                  showOnInputClick={true}
-                  style = {{width: "30px", display: "inline-block", marginLeft: "5px"}}
-              />
+            <Row style = {{textAlign: "center", marginTop: "28px", textAlign: "center"}}>
+              <Col>
+                <h6 style = {{display: "inline-block", color: "#707070"}}>Filter by</h6>
+                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} style = {{display: "inline-block", marginLeft: "5px"}}>
+                  <DropdownToggle caret style = {{color: "#707070", backgroundColor: "#FFFFFF", borderColor: "#CECECE"}}>
+                    Filter by
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem>Foo Action</DropdownItem>
+                    <DropdownItem>Bar Action</DropdownItem>
+                    <DropdownItem>Quo Action</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
               </Col>
             </Row>
-          </Col>
-          <Col md="12" sm="12" lg="12" style = {{marginTop: "27px"}}>
-            <Button color="link" style = {{display: "inline-block", color: "#137991", backgroundColor: "#FFFFFF"}}>Reset</Button>
-            <Button style = {{display: "inline-block", color: "#707070", backgroundColor: "#FFFFFF", borderColor: "#CECECE", position: "relative", float: "right", width: "120px"}}>SEARCH</Button>{' '}
-          </Col>
-          <Col>
-            <Nav style={{marginTop: "15px", display: "block"}} vertical>
-              <DropdownItem divider></DropdownItem>
-              <DropdownItem style = {{family:"Nunito Sans", size: "16"}} header>Applications</DropdownItem>
-              <SidebarNavItem style = {{family:"Nunito Sans", size: "16", color: "#707070"}} logo_path={GCcollab} text="GCcollab" path="/newcollab" />
-              <SidebarNavItem logo_path={GCconnex} text="GCconnex"/>
-              <SidebarNavItem logo_path={GCtools} text="Career Marketplace" path="/collab-careermp" />
-              <SidebarNavItem style = {{color: "#707070"}} logo_path={GCmessage} text="GCmessage" />
-              <SidebarNavItem color= "secondary" logo_path={GCpedia} text="GCpedia"/>
-            </Nav>
+
+            {/* Date pickers */}
+            <Row style = {{marginTop: "27px"}}>
+              <Col style = {{display: "inline-block"}}>
+                <h6 style = {{color: "#707070", display: "inline-block"}}>From</h6>
+                <DatePickerInput
+                    displayFormat='DD/MM/YYYY'
+                    returnFormat='YYYY-MM-DD'
+                    className='my-react-component'
+                    placeholder='Start Date'
+                    defaultValue={this.state.startDate}
+                    iconClassName='calendar icon'
+                    validationFormat="DD/MM/YYYY"
+                    showOnInputClick={true}
+                    style = {{width: "40%", display: "inline-block", marginLeft: "5px"}}
+                />
+              </Col>
+              <Col style = {{display: "inline-block"}}>
+                <h6 style = {{color: "#707070", display: "inline-block"}}>To</h6>
+                <DatePickerInput
+                    displayFormat='DD/MM/YYYY'
+                    returnFormat='YYYY-MM-DD'
+                    className='my-react-component'
+                    placeholder='End Date'
+                    defaultValue={this.state.endDate}
+                    iconClassName='calendar icon'
+                    validationFormat="DD/MM/YYYY"
+                    showOnInputClick={true}
+                    style = {{width: "40%", display: "inline-block", marginLeft: "5px"}}
+                />
+              </Col>
+            </Row>
+
+            {/* Reset and Search Buttons */}
+            <Row>
+              <Col md="12" sm="12" lg="12" style = {{marginTop: "27px"}}>
+                <Button color="link" style = {{display: "inline-block", color: "#137991", backgroundColor: "#FFFFFF"}}>Reset</Button>
+                <Button style = {{display: "inline-block", color: "#707070", backgroundColor: "#FFFFFF", borderColor: "#CECECE", position: "relative", float: "right", width: "120px"}}>SEARCH</Button>{' '}
+              </Col>
+            </Row>
+            
+            {/* Navigation between GCTools */}
+            <Row>
+              <Col>
+                <Nav style={{marginTop: "15px", display: "block"}} vertical>
+                  <DropdownItem divider></DropdownItem>
+                  <DropdownItem style = {{family:"Nunito Sans", size: "16"}} header>Applications</DropdownItem>
+                  <SidebarNavItem style = {{family:"Nunito Sans", size: "16", color: "#707070"}} logo_path={GCcollab} text="GCcollab" path="/newcollab" />
+                  <SidebarNavItem logo_path={GCconnex} text="GCconnex"/>
+                  <SidebarNavItem logo_path={GCtools} text="Career Marketplace" path="/collab-careermp" />
+                  <SidebarNavItem style = {{color: "#707070"}} logo_path={GCmessage} text="GCmessage" />
+                  <SidebarNavItem color= "secondary" logo_path={GCpedia} text="GCpedia"/>
+                </Nav>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
